0944a378c3aa0d7a5502626fa07ac83c34bdbaf9
SANDEEP YADAV <36203125+SANDEEP0181@users.noreply.github.com>
2026-09-07 20:57:05 +0530

    Add TrendCart offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='trendcart-v1';
+const APP_SHELL=['./','./index.html','./style.css','./script.js','./size-fix.js','./cart.html','./checkout.html','./wishlist.html','./track-order.html','./account.html','./support.html','./order-confirmation.html','./invoice.html','./404.html','./site.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const request=event.request;if(request.method!=='GET')return;event.respondWith(fetch(request).then(response=>{if(response.ok&&new URL(request.url).origin===self.location.origin){const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(request,copy))}return response}).catch(()=>caches.match(request).then(cached=>cached||caches.match('./404.html'))))});
